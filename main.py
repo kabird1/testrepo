6343530aa1770229cf0851095d0c4d220ab27fd3
kabird1 <148470042+kabird1@users.noreply.github.com>
2023-12-28 17:03:29 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -100,9 +100,9 @@ if 'weights' not in st.session_state:
     st.session_state.weights = None
 
 #user uploads csv with their labelled data
-if st.session_state.input_data==None:
+if st.session_state.input_data.empty():
     st.session_state.input_data=st.file_uploader('Upload your training data', type=['.csv'], help='Upload the training dataset. It must include the following 3 columns: latitude, longitude and features')
-if st.session_state.input_data!=None and st.session_state.model==None:
+if st.session_state.input_data.empty()==False and st.session_state.model==None:
     #once user uploaded csv, load into pandas dataframe, append images from google maps api, split into training and validation set
     st.session_state.input_data=pd.read_csv(st.session_state.input_data)
     with st.spinner('Preprocessing data for model training'):
