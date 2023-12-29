e0ee4f7649c29d291da5d6b97b4acae415d89d3a
kabird1 <148470042+kabird1@users.noreply.github.com>
2023-12-29 15:04:55 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -164,8 +164,8 @@ if st.session_state.input_file!=None and st.session_state.model==None:
         st.session_state.weights = st.session_state.model.get_weights()
 if st.session_state.input_file!=None and st.session_state.model!=None:
     col1, col2 = st.columns(2)
-    col1.st.download_button('Download model architecture', data=st.session_state.json_config, file_name='json_config.json', use_container_width=True)
-    col2.st.download_button('Download model weights', data=str(st.session_state.weights), file_name='weights.txt', use_container_width=True)
+    col1.download_button('Download model architecture', data=st.session_state.json_config, file_name='json_config.json', use_container_width=True)
+    col2.download_button('Download model weights', data=str(st.session_state.weights), file_name='weights.txt', use_container_width=True)
 
 
     
