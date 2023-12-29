4ebb0a0094f18e1ac9dfd0169f5047003fb2c278
kabird1 <148470042+kabird1@users.noreply.github.com>
2023-12-29 15:50:56 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -163,7 +163,7 @@ if st.session_state.input_file!=None and st.session_state.model==None:
 
         st.session_state.json_config = st.session_state.model.to_json()
         st.session_state.weights = st.session_state.model.get_weights()
-        st.session_state.weights = pickle.dumps(st.session_state.model_get_weights)
+        st.session_state.weights = pickle.dumps(st.session_state.weights)
 if st.session_state.input_file!=None and st.session_state.model!=None:
     col1, col2 = st.columns(2)
     col1.download_button('Download model architecture', data=str(st.session_state.json_config), file_name='architecture.txt', use_container_width=True)
